a2ac128ad00ac39dc2dc497106ba1df9a55b249d
Cody C <ccoane@gmail.com>
2019-04-14 18:20:32 -0500

    This is a hot dumpster fire of a commit.  Added RF24 in working fashion.  Only the Transmitter will attempt to do any REST calls and connect to Wifi.  The receiver will be in permanent listening mode. The button clicks must be changed as the pins they were occupying are the HSPI pins which are necessary for any RF24 use to work.

--- nerfArduinoStatusTracker/nerfArduinoStatusTracker.ino
+++ nerfArduinoStatusTracker/nerfArduinoStatusTracker.ino
@@ -11,21 +11,35 @@
 #include <Adafruit_SSD1306.h>
 #include <splash.h>
 #include <ArduinoJson.h>  //https://arduinojson.org/v5/assistant/
-
-#include <SPI.h>  
+//////////////////////////////////////
+#include <SPI.h>
 #include "RF24.h"
-byte addresses[][6] = {"0"};
-RF24 myRadio (D3, D4);
-// TODO: Move this struct into a Header file
-struct kothTeamStruct
-{
-  int teamId;
-  int points;
-};
 
-typedef struct kothTeamStruct KothTeamStruct;
-KothTeamStruct KothTeamPayload;
+byte addresses[][6] = {"1Node","2Node"};
 
+
+/****************** User Config ***************************/
+/***      Set this radio as radio number 0 or 1         ***/
+bool radioNumber = 1;
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(D3,D4);
+/**********************************************************/
+
+
+// Used to control whether this node is sending or receiving
+bool role = radioNumber;
+
+/**
+* Create a data structure for transmitting and receiving data
+* This allows many variables to be easily sent and received in a single transmission
+* See http://www.cplusplus.com/doc/tutorial/structures/
+*/
+struct dataStruct{
+  int redTeam = 0;
+  int blueTeam = 0;
+}myData;
+///////////////////////////////////////
 #define RED_TEAM 0                                                          
 #define BLUE_TEAM 1                                                     
 #define RESET 2 
@@ -35,7 +49,6 @@ KothTeamStruct KothTeamPayload;
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     3 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
 
 int SCL_PIN = D1; // D1 - Not used in code.  Just for reference when connecting pins to Display.
 int SDA_PIN = D2; //D2 - Not used in code.  Just for reference when connecting pins to Display.
@@ -54,61 +67,79 @@ const char* password = "PW";
 
 WiFiClient client;
 HTTPClient http;
+
 void setup() {
   Serial.begin(115200);
   delay(10);
 
+  Serial.println("STARTING");
   ///////////////RF24 Setup/////////////////
-  Serial.println("Testing radio");
-  myRadio.begin();  
-  myRadio.setChannel(115); 
-  myRadio.setPALevel(RF24_PA_MAX);
-  myRadio.setDataRate( RF24_250KBPS ) ; 
-  myRadio.openWritingPipe( addresses[0]);
-  delay(1000);
+  Serial.println("RF24/examples/GettingStarted_HandlingData");
+  Serial.println("*** PRESS 'T' to begin transmitting to the other node");
+  
+  radio.begin();
+
+  radio.setPALevel(RF24_PA_MAX);           // If you want to save power use "RF24_PA_MIN" but keep in mind that reduces the module's range
+  radio.setDataRate(RF24_250KBPS );
+  radio.setRetries(15,15);                // Max delay between retries & number of retries
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+  }else{
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1,addresses[1]);
+  }
+  
+//  myData.value = 1.22;
+  // Start the radio listening for data
+  radio.startListening();
   /////////////////////////////////////////
 
-  pinMode(redTeamPin, INPUT_PULLUP);
-  pinMode(blueTeamPin, INPUT_PULLUP);
-  pinMode(resetPin, INPUT_PULLUP);
+//  pinMode(redTeamPin, INPUT_PULLUP);
+//  pinMode(blueTeamPin, INPUT_PULLUP);
+//  pinMode(resetPin, INPUT_PULLUP);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
   display.clearDisplay();
   
   // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  display.clearDisplay();
-  display.setCursor(0,0);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  String preConnectionDisplayText = "Connecting: " + String(ssid);
-  display.println(preConnectionDisplayText);
-  display.display();
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  if (role) {
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    
+    WiFi.begin(ssid, password);
     display.clearDisplay();
     display.setCursor(0,0);
     display.setTextSize(1);
     display.setTextColor(WHITE);
-    preConnectionDisplayText += ".";
+    String preConnectionDisplayText = "Connecting: " + String(ssid);
     display.println(preConnectionDisplayText);
     display.display();
+    
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      preConnectionDisplayText += ".";
+      display.println(preConnectionDisplayText);
+      display.display();
+    }
+    
+    Serial.println("");
+    Serial.println("WiFi connected");  
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    DiscoverBaseUrl();
   }
   
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  
-//  DiscoverBaseUrl();
 }
 
 // This is a hack for my phone's hotspot connection as the arduino needs to find whatever
@@ -145,18 +176,23 @@ void DiscoverBaseUrl () {
 int value = 0;
 
 void loop() {
-  int _redTeamPin = digitalRead(redTeamPin);
-  int _blueTeamPin = digitalRead(blueTeamPin);
-  int _resetPin = digitalRead(resetPin);
+//  int _redTeamPin = digitalRead(redTeamPin);
+//  int _blueTeamPin = digitalRead(blueTeamPin);
+//  int _resetPin = digitalRead(resetPin);
   
-  if (_redTeamPin == LOW) {          
-    processGetRequest(RED_TEAM);
-  } else if (_blueTeamPin == LOW) {          
-    processGetRequest(BLUE_TEAM);
-  } /*else if (_resetPin == LOW) {          
-    processGetRequest(RESET);
-  }*/
-  processGetRequest(STATUS);
+
+//  if (_redTeamPin == LOW) {          
+//    processGetRequest(RED_TEAM);
+//  } else if (_blueTeamPin == LOW) {          
+//    processGetRequest(BLUE_TEAM);
+//  } /*else if (_resetPin == LOW) {          
+//    processGetRequest(RESET);
+//  }*/
+  if (role) {
+    delay(250);
+    processGetRequest(STATUS);
+  }
+  ProcessRadio();
 }
 
 void processGetRequest ( int action ) {
@@ -212,28 +248,143 @@ void displayStatus (String jsonVal) {
   const char* Teams_0_teamName = Teams_0["teamName"]; // "Red"
   bool Teams_0_isActive = Teams_0["isActive"]; // false
   int Teams_0_elapsedTimeInSeconds = Teams_0["elapsedTimeInSeconds"]; // 0
+  myData.redTeam = Teams_0_elapsedTimeInSeconds;
   
   JsonObject& Teams_1 = Teams[1];
   const char* Teams_1_teamName = Teams_1["teamName"]; // "Blue"
   bool Teams_1_isActive = Teams_1["isActive"]; // false
   int Teams_1_elapsedTimeInSeconds = Teams_1["elapsedTimeInSeconds"]; // 0
+  myData.blueTeam = Teams_1_elapsedTimeInSeconds;
 
-  display.clearDisplay();
-//  display.setCursor(0,18);
-//  display.setTextSize(2);
+//  display.clearDisplay();
+//  display.setCursor(36,0);
 //  display.setTextColor(WHITE);
-//  display.println("Red: " + String(Teams_0_elapsedTimeInSeconds));
-//  display.println("Blue: " + String(Teams_1_elapsedTimeInSeconds));
-  display.setCursor(36,0);
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.println("CONNECTED");
-  display.setTextSize(2);
-  display.setCursor(0,16);
-  display.println(" RED  BLUE");
-  display.setCursor(10,32);
-  display.print(String(Teams_0_elapsedTimeInSeconds));
-  display.setCursor(72,32);
-  display.print(String(Teams_1_elapsedTimeInSeconds));
-  display.display();
+//  display.setTextSize(1);
+//  display.println("CONNECTED");
+//  display.setTextSize(2);
+//  display.setCursor(0,16);
+//  display.println(" RED  BLUE");
+//  display.setCursor(10,32);
+//  display.print(String(myData.redTeam));
+//  display.setCursor(72,32);
+//  display.print(String(myData.blueTeam));
+//  display.display();
+}
+
+void ProcessRadio () {
+  if (role == 1)  {
+    
+    radio.stopListening();                                    // First, stop listening so we can talk.
+    
+    delay(250);
+    
+    Serial.println(String("Now sending"));
+
+//    myData.redTeam = micros();
+     if (!radio.write( &myData, sizeof(myData) )){
+       Serial.println(String("failed"));
+     }
+        
+    radio.startListening();                                    // Now, continue listening
+    
+    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    
+    while ( ! radio.available() ){                             // While nothing is received
+      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+          timeout = true;
+          break;
+      }      
+    }
+        
+    if ( timeout ){                                             // Describe the results
+        Serial.println(String("Failed, response timed out."));
+    }else{
+                                                                // Grab the response, compare, and send to debugging spew
+        radio.read( &myData, sizeof(myData) );
+        unsigned long time = micros();
+        
+        // Spew it
+//        Serial.print(String("Sent "));
+//        Serial.print(time);
+//        Serial.print(String(", Got response "));
+//        Serial.print(myData.redTeam);
+//        Serial.print(String(", Round-trip delay "));
+//        Serial.print(time-myData.redTeam);
+//        Serial.print(String(" microseconds Value "));
+//        Serial.println(myData.value);
+        display.clearDisplay();
+        display.setCursor(36,0);
+        display.setTextColor(WHITE);
+        display.setTextSize(1);
+        display.println("CONNECTED");
+        display.setTextSize(2);
+        display.setCursor(0,16);
+        display.println(" RED  BLUE");
+        display.setCursor(10,32);
+        display.print(String(myData.redTeam));
+        display.setCursor(72,32);
+        display.print("123");
+        display.display();
+    }
+
+    // Try again 1s later
+  }
+
+
+
+/****************** Pong Back Role ***************************/
+
+  if ( role == 0 )
+  {
+    if( radio.available()){
+                                                           // Variable for the received timestamp
+      while (radio.available()) {                          // While there is data ready
+        radio.read( &myData, sizeof(myData) );             // Get the payload
+      }
+     
+      radio.stopListening();                               // First, stop listening so we can talk  
+//      myData.value += 0.01;                                // Increment the float value
+      radio.write( &myData, sizeof(myData) );              // Send the final one back.      
+      radio.startListening();                              // Now, resume listening so we catch the next packets.     
+      Serial.println("Sent response " + String(myData.redTeam) + ":" + String(myData.blueTeam));
+//      Serial.print(myData.redTeam);  
+//      Serial.print(String(" : "));
+//      Serial.println(myData.value);
+      display.clearDisplay();
+      display.setCursor(36,0);
+      display.setTextColor(WHITE);
+      display.setTextSize(1);
+      display.println("CONNECTED");
+      display.setTextSize(2);
+      display.setCursor(0,16);
+      display.println(" RED  BLUE");
+      display.setCursor(10,32);
+      display.print(String(myData.redTeam));
+      display.setCursor(72,32);
+      display.print(String(myData.blueTeam));
+      display.display();
+   }
+ }
+
+
+
+
+/****************** Change Roles via Serial Commands ***************************/
+
+  if ( Serial.available() )
+  {
+    char c = toupper(Serial.read());
+    if ( c == 'T' && role == 0 ){      
+      Serial.print(String("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
+      role = 1;                  // Become the primary transmitter (ping out)
+    
+   }else
+    if ( c == 'R' && role == 1 ){
+      Serial.println(String("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
+       role = 0;                // Become the primary receiver (pong back)
+       radio.startListening();
+       
+    }
+  }
 }
